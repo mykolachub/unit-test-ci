3744828e5a088f86c95977ea55a3d053ada1454b
Nikolai <nikolaichub.me@gmail.com>
2022-02-22 19:35:46 +0200

    Added initial structure of linked list

--- index.js
+++ index.js
@@ -14,4 +14,39 @@ class List {
     this.tail = null;
     this.length = 0;
   }
+
+  // Amount of element in the list
+  length() {}
+
+  // Add element to end
+  append(data) {}
+
+  // Paste element into certain position
+  insert(data, idx) {}
+
+  // Delete element by id
+  delete(idx) {}
+
+  // Delete elements by data
+  deleteAll(data) {}
+
+  // Get element by id
+  get(idx) {}
+
+  // Reverse whole list
+  reverse() {}
+
+  // Get head element
+  findFirst() {}
+
+  // Get tail element
+  findLast() {}
+
+  // Clear whole list
+  clear() {}
+
+  // Extend existing list with another one
+  extend(list) {}
+
+  clone() {}
 }

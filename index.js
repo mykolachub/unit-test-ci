e5a005e3f00245eba69be8a88ca2dd44d90306b8
Nikolai <nikolaichub.me@gmail.com>
2022-02-22 19:51:56 +0200

    Added linked list export

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 'use strict';
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-    this.prev = null;
-  }
-}
+const List = require('./lib/list');
 
-class List {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  // Amount of element in the list
-  length() {}
-
-  // Add element to end
-  append(data) {}
-
-  // Paste element into certain position
-  insert(data, idx) {}
-
-  // Delete element by id
-  delete(idx) {}
-
-  // Delete elements by data
-  deleteAll(data) {}
-
-  // Get element by id
-  get(idx) {}
-
-  // Reverse whole list
-  reverse() {}
-
-  // Get head element
-  findFirst() {}
-
-  // Get tail element
-  findLast() {}
-
-  // Clear whole list
-  clear() {}
-
-  // Extend existing list with another one
-  extend(list) {}
-
-  clone() {}
-}
+module.exports = List;

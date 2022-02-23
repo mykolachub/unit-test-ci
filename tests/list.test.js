0d2cdd9fca8ca08ca1887f9e4911057dcc1f9697
Nikolai <nikolaichub.me@gmail.com>
2022-02-23 19:43:56 +0200

    test: basic test file structure

--- tests/list.test.js
+++ tests/list.test.js
@@ -2,3 +2,29 @@
 
 const assert = require('assert').strict;
 const List = require('../lib/list');
+
+describe('Doubly Linked List', () => {
+  it('should append element', () => {});
+
+  it('should insert element', () => {});
+
+  it('should delete element', () => {});
+
+  it('should delete all elements by value', () => {});
+
+  it('should clone list', () => {});
+
+  it('should reverse list', () => {});
+
+  it('should clear whole list', () => {});
+
+  it('should extend list by another one', () => {});
+
+  it('should return element by id ', () => {});
+
+  it('should return list length', () => {});
+
+  it('should return first element by value', () => {});
+
+  it('should return last element by value', () => {});
+});
